514227d01a0ca7c690ed5b083ba11f7b2ba269e8
CryptoSOL123 <163804105+CryptoSOL123@users.noreply.github.com>
2025-04-05 15:35:02 -0700

    dont use this

---
+++ Assets/Scripts/EnemySpike.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using UnityEngine;
+
+public class EnemySpike : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Player died!");
+        }
+    }
+}
