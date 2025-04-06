514bd1436126b152af52e7ce470fac13fc353ecf
Max <maxshi239@gmail.com>
2025-04-05 23:50:28 -0700

    muffled sound when underwater

--- Assets/PersistentAudio.cs
+++ Assets/PersistentAudio.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PersistentAudio : MonoBehaviour
 {
+    public AudioSource backgroundMusic;
     void Awake()
     {
         // If another copy exists, destroy this one (prevents duplicates on reload)
@@ -12,5 +13,16 @@ public class PersistentAudio : MonoBehaviour
         }
 
         DontDestroyOnLoad(gameObject);
+    }
+    void Update()
+    {
+        if (PlayerStateManager.currentPlayerState == PlayerStateManager.PlayerState.Water)
+        {
+            backgroundMusic.pitch = 0.8f;
+        }
+        else if (PlayerStateManager.currentPlayerState == PlayerStateManager.PlayerState.Land)
+        {
+            backgroundMusic.pitch = 1.0f;
+        }
     }
 }
